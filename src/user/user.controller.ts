c24685467b2c3d8a8ba6a429c407c101b926c56a
Anish Lamichhane <codingonly31@gmail.com>
2024-05-15 21:37:21 +0545

    day two succesful

--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -1,4 +1,30 @@
-import { Controller } from '@nestjs/common';
+import { UsermainService } from './serive/usermain.service';
+import { Body, Controller ,HttpCode,HttpStatus,Post} from '@nestjs/common';
+import { singupDto } from './dto/user.dto';
+import { UserService } from './serive/user.service';
+import { loginDto } from './dto/login.dto';
 
 @Controller('user')
-export class UserController {}
+export class UserController {
+
+    constructor( private UserService : UserService, private UsermainService : UsermainService){
+
+    }
+
+  @Post('singup')
+  async create(@Body() singupDto: singupDto){
+
+        return this.UsermainService.signup( singupDto);
+}
+
+
+@HttpCode(HttpStatus.OK)
+@Post('login')
+login(@Body() dto: loginDto) {
+  return this.UsermainService.login(dto);
+}
+
+
+
+
+}
