8cfa7fb652f600e75c886ed26110be7ca8c2fdc4
Anish Lamichhane <codingonly31@gmail.com>
2024-05-20 20:47:38 +0545

    adding jwt module

--- src/user/user.module.ts
+++ src/user/user.module.ts
@@ -1,10 +1,18 @@
 import { Module } from '@nestjs/common';
 import { UserController } from './user.controller';
 import { UsermainService } from './serive/usermain.service';
-import { EmailService } from './serive/email.service';
+import { JwtModule } from '@nestjs/jwt';
 
 @Module({
+  imports:[
+    JwtModule.register({
+      secret: process.env.JWT_SECRET,
+      signOptions: { expiresIn: process.env.JWT_LIFETIME },
+    }),
+  ],
+  
   controllers: [UserController ],
-  providers: [ UsermainService, EmailService]
+  providers: [ UsermainService],
+  exports:[UsermainService]
 })
 export class UserModule {}
