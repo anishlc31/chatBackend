d951a185acfcf340637995cc1f96d58cc3789995
Anish Lamichhane <codingonly31@gmail.com>
2024-05-20 20:43:15 +0545

    chagne for middleware

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,11 +1,27 @@
-import { Module } from '@nestjs/common';
+import { MiddlewareConsumer, Module, RequestMethod } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { UserModule } from './user/user.module';
+import { AuthModule } from './auth/auth.module';
+import { MongooseModule } from '@nestjs/mongoose';
+import { AuthMiddleware } from './milddlerware/auth.middleware';
 
 @Module({
-  imports: [UserModule],
+  imports: [UserModule, AuthModule,
+   // MongooseModule.forRoot('mongodb://localhost/nest'), // Change this to your MongoDB connection string
+
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
-export class AppModule {}
+export class AppModule {
+  configure(consumer: MiddlewareConsumer) {
+    consumer
+      .apply(AuthMiddleware)
+      .exclude(
+        { path: '/api/users', method: RequestMethod.POST},
+        {path: '/api/users/login', method: RequestMethod.POST}
+      )
+      .forRoutes('')
+  }
+}
