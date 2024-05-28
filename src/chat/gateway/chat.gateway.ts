3e8defe6ae991c15a349bfdc9b08c4651066d956
Anish Lamichhane <codingonly31@gmail.com>
2024-05-28 21:56:43 +0545

    till now and it will be updated tommorow

--- src/chat/gateway/chat.gateway.ts
+++ src/chat/gateway/chat.gateway.ts
@@ -1,9 +1,42 @@
-import { SubscribeMessage, WebSocketGateway } from '@nestjs/websockets';
+import { OnGatewayConnection, OnGatewayDisconnect, SubscribeMessage, WebSocketGateway, WebSocketServer } from '@nestjs/websockets';
+import { Server, Socket } from 'socket.io';
+import { AuthService } from 'src/auth/auth.service';
 
 @WebSocketGateway({ cors: { origin: ['https://hoppscotch.io', 'http://localhost:3000', 'http://localhost:4200'] } })
-export class ChatGateway {
-  @SubscribeMessage('message')
-  handleMessage(client: any, payload: any): string {
-    return 'Hello world!';
-  }
+export class ChatGateway implements OnGatewayConnection ,OnGatewayDisconnect {
+  
+    constructor (private authSerive : AuthService){
+
+    }
+
+  @WebSocketServer()
+  server: Server;
+
+  async handleConnection(socket : Socket) {
+
+    try{
+
+        const decodedToken = await this.authSerive.verifyJwt(socket.handshake.headers.authorization);
+
+
+    }catch{
+
+    }
+}
+
+
+  handleDisconnect(client: any) {
+    throw new Error('Method not implemented.');
+}
+
+
+
+@SubscribeMessage('message')
+handleMessage(client: any, payload: any): string {
+  return 'Hello world!';
+}
+
+
+
+
 }
