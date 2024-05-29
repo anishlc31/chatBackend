18802ec101738e99f19bc2d26dfae02a60b16dac
Anish Lamichhane <codingonly31@gmail.com>
2024-05-29 21:37:21 +0545

    ikd its right or wrong

---
+++ src/chat/user-extractor-service.service.ts
@@ -0,0 +1,21 @@
+import { Injectable, UnauthorizedException } from '@nestjs/common';
+
+import { Socket } from 'socket.io';
+import { AuthService } from 'src/auth/auth.service';
+import { JwtPayload } from 'src/auth/types';
+
+@Injectable()
+export class UserExtractorService {
+  constructor(private readonly authService: AuthService) {}
+
+  async extractUserId(socket: Socket): Promise<string> {
+    const token = socket.handshake.headers.authorization;
+    if (!token) {
+      throw new UnauthorizedException('Token not provided');
+    }
+
+    const decodedToken = await this.authService.verifyJwt(token);
+    const user = decodedToken as JwtPayload;
+    return user.sub;
+  }
+}
