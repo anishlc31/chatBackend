74bdc7ca503f02c5600ac26fc2f615717c638b03
Anish Lamichhane <codingonly31@gmail.com>
2024-06-10 21:47:10 +0545

    change for idk , to remove error

--- src/chat/user-extractor-service.service.ts
+++ src/chat/user-extractor-service.service.ts
@@ -1,5 +1,4 @@
 import { Injectable, UnauthorizedException } from '@nestjs/common';
-
 import { Socket } from 'socket.io';
 import { AuthService } from 'src/auth/auth.service';
 import { JwtPayload } from 'src/auth/types';
@@ -9,14 +8,21 @@ export class UserExtractorService {
   constructor(private readonly authService: AuthService) {}
 
   async extractUserId(socket: Socket): Promise<string> {
-    const token = socket.handshake.headers.authorization;
+    const authHeader = socket.handshake.headers.authorization;
+    if (!authHeader) {
+      throw new UnauthorizedException('Token not provided');
+    }
+
+    const token = authHeader.split(' ')[1]; // Extract the token after 'Bearer'
     if (!token) {
       throw new UnauthorizedException('Token not provided');
     }
 
+    console.log('Extracted Token:', token);
+
     const decodedToken = await this.authService.verifyJwt(token);
     const user = decodedToken as JwtPayload;
-    console.log(user)
+    console.log(user);
     return user.sub;
   }
 }
