b5ea55981c8a1f24a89c383053b53db8ec449e5b
Anish Lamichhane <codingonly31@gmail.com>
2024-06-12 22:52:29 +0545

    somethings happen

--- src/chat/gateway/chat.gateway.ts
+++ src/chat/gateway/chat.gateway.ts
@@ -17,37 +17,61 @@ export class ChatGateway implements OnGatewayConnection, OnGatewayDisconnect {
   @WebSocketServer()
   server: Server;
 
-  async handleConnection(socket: Socket) {
-    try {
-      console.log('Headers:', socket.handshake.headers);
-      const userId = await this.userExtractorService.extractUserId(socket);
-      console.log('User ID:', userId);
+  // async handleConnection(socket: Socket) {
+  //   try {
+  //     // console.log('Headers:', socket.handshake.headers);
+  //     const userId = await this.userExtractorService.extractUserId(socket);
+  //     console.log('User ID:', userId);
+  
+  //     if (!userId) {
+  //       return this.disconnect(socket);
+  //     } else {
+  //       socket.data.user = userId;
+  //       console.log('Socket data after setting user:', socket.data);
+  //       const room = await this.roomService.getRoomForUser(userId, { page: 1, limit: 10 });
+  //       return this.server.to(socket.id).emit('rooms', room);
+  //     }
+  //   } catch (error) {
+  //     console.error('Connection Error:', error);
+  //     return this.disconnect(socket);
+  //   }
+  // }
+  
 
-      if (!userId) {
-        return this.disconnect(socket);
-      } else {
-        socket.data.user = userId;
-        const room = await this.roomService.getRoomForUser(userId, { page: 1, limit: 10 });
-        return this.server.to(socket.id).emit('rooms', room);
-      }
-    } catch (error) {
-      console.error('Connection Error:', error);
-      return this.disconnect(socket);
-    }
+  // handleDisconnect(client: Socket) {
+  //   //console.log(`Client disconnected: ${client.id}`);
+  // }
+
+  // private disconnect(socket: Socket) {
+  //   socket.emit('Error', new UnauthorizedException());
+  //   socket.disconnect();
+  // }
+
+
+
+
+
+  // @SubscribeMessage('createRoom')
+  // async onCreateRoom(socket: Socket, room: string) {
+  //   console.log('Socket ID:', socket.data.user);
+  //   return this.roomService.createRoom(room, socket.data.user);
+  // }
+
+
+  @SubscribeMessage('message')
+  handleMessage(client:any , payload : any )  {
+ this.server.emit('message', 'test ')
   }
 
-  handleDisconnect(client: Socket) {
-    console.log(`Client disconnected: ${client.id}`);
+
+  handleConnection(client: any, ...args: any[]) {
+    console.log('connected ')
+    this.server.emit('message', 'test ')
+
   }
 
-  private disconnect(socket: Socket) {
-    socket.emit('Error', new UnauthorizedException());
-    socket.disconnect();
+   handleDisconnect(client: Socket) {
+    console.log('disconnected ')
   }
-
-  @SubscribeMessage('createRoom')
-  async onCreateRoom(socket: Socket, room: string) {
-    console.log('Socket ID:', socket.data.user);
-    return this.roomService.createRoom(room, socket.data.user);
-  }
+  
 }
