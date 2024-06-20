8829dc469929ab5cd5c1db745658f71a477d0346
Anish Lamichhane <codingonly31@gmail.com>
2024-06-20 21:49:49 +0545

    adding get user by username

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,5 +1,5 @@
 import { AuthService } from './auth.service';
-import { Body, Controller, HttpCode, HttpStatus, Post, Req, UseGuards,Request } from '@nestjs/common';
+import { Body, Controller, HttpCode, HttpStatus, Post, Req, UseGuards,Request, Get, Query } from '@nestjs/common';
 import { loginDto, singupDto } from './dto';
 import { JwtAuthGuard } from './guards/jwt.guards';
 import { GetCurrentUserId  } from './decorator/user.decorator';
@@ -23,15 +23,20 @@ export class AuthController {
     @Post ('/login')
     login(@Body() dto  : loginDto){
         return this.authService.login(dto);
-
     }
 
 
+    @Get(':id')
     @HttpCode(HttpStatus.OK)
-    @UseGuards(JwtAuthGuard)
     @Post ('/logout')
     logout(@GetCurrentUserId() userId: string){
 return this.authService.logout(userId)
         
     }
+
+
+    @Get('/find-by-username')
+  async findAllByUsername(@Query('username') username: string) {
+    return this.authService.findAllByUsername(username);
+  }
 }
