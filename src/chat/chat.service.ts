bd9fccf38e25c466b02e734cc28f3b8ea0f7f8e0
Anish Lamichhane <codingonly31@gmail.com>
2024-06-24 23:30:47 +0545

    second day completly

--- src/chat/chat.service.ts
+++ src/chat/chat.service.ts
@@ -1,50 +1,34 @@
 import { Injectable } from '@nestjs/common';
 import { PrismaClient } from '@prisma/client';
-import { IPaginationOptions } from 'nestjs-typeorm-paginate';
 
 const prisma = new PrismaClient();
 
-
 @Injectable()
 export class ChatService {
+  
+  async sendMessage(senderId: string, receiverId: string, content: string) {
+    return prisma.message.create({
+      data: {
+        content,
+        senderId,
+        receiverId,
+      },
+    });
+  }
 
-    async sendMessage(senderId: string, receiverId: string, content: string) {
-        return prisma.message.create({
-          data: {
-            content,
-            senderId,
-            receiverId,
-          },
-        });
-      }
-
-
-      async getMessagesForUser(userId: string) {
-        return prisma.message.findMany({
-          where: {
-            OR: [
-              { senderId: userId },
-              { receiverId: userId },
-            ],
-          },
-          orderBy: {
-            createdAt: 'asc',
-          },
-        });
-      }
-    
-
-      async getMessagesBetweenUsers(user1Id: string, user2Id: string) {
-        return prisma.message.findMany({
-          where: {
-            OR: [
-              { senderId: user1Id, receiverId: user2Id },
-              { senderId: user2Id, receiverId: user1Id },
-            ],
-          },
-          orderBy: {
-            createdAt: 'asc',
-          },
-        });
-      }
+  async getMessagesBetweenUsers(user1Id: string, user2Id: string, skip: number, take: number) {
+    return prisma.message.findMany({
+      where: {
+        OR: [
+          { senderId: user1Id, receiverId: user2Id },
+          { senderId: user2Id, receiverId: user1Id },
+        ],
+      },
+      orderBy: {
+        createdAt: 'desc', // Fetch most recent messages first
+      },
+      skip: skip,
+      take: take,
+    });
+  }
 }
