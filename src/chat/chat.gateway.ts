fd154e3ac1f2bf113389a2790d91563aa126867c
Anish Lamichhane <codingonly31@gmail.com>
2024-06-26 14:43:54 +0545

    msg deliver done

--- src/chat/chat.gateway.ts
+++ src/chat/chat.gateway.ts
@@ -23,19 +23,6 @@ export class ChatGateway implements OnGatewayConnection, OnGatewayDisconnect {
     //console.log(`Client disconnected: ${client.id}`);
   }
 
-  @SubscribeMessage('sendMessage')
-  async handleMessage(
-    @MessageBody() data: { senderId: string; receiverId: string; content: string },
-    @ConnectedSocket() client: Socket,
-  ) {
-    const message = await this.chatService.sendMessage(data.senderId, data.receiverId, data.content);
-    this.server.to(data.receiverId).emit('receiveMessage', message);
-    this.server.to(data.senderId).emit('updateUserList', { userId: data.receiverId });
-    this.server.to(data.receiverId).emit('updateUserList', { userId: data.senderId });
-    return message;
-  }
-  
-
 
   @SubscribeMessage('joinRoom')
   handleJoinRoom(
@@ -46,15 +33,50 @@ export class ChatGateway implements OnGatewayConnection, OnGatewayDisconnect {
     console.log(`Client ${client.id} joined room ${userId}`);
   }
 
-  @SubscribeMessage('getMessages')
+  @SubscribeMessage('sendMessage')
+  async handleMessage(
+    @MessageBody() data: { senderId: string; receiverId: string; content: string },
+    @ConnectedSocket() client: Socket,
+  ) {
+    const message = await this.chatService.sendMessage(data.senderId, data.receiverId, data.content);
+    this.server.to(data.receiverId).emit('receiveMessage', message);
+    this.server.to(data.senderId).emit('updateMessageStatus', { messageId: message.id, status: 'delivered' });
+    this.server.to(data.senderId).emit('updateUserList', { userId: data.receiverId });
+    this.server.to(data.receiverId).emit('updateUserList', { userId: data.senderId });
+    await this.chatService.markMessageAsDelivered(message.id);
+
+    return message;
+  }
+
+  
+ @SubscribeMessage('getMessages')
   async handleGetMessages(
     @MessageBody() data: { user1Id: string; user2Id: string; skip: number; take: number },
     @ConnectedSocket() client: Socket,
   ) {
     const messages = await this.chatService.getMessagesBetweenUsers(data.user1Id, data.user2Id, data.skip, data.take);
+    
     await this.chatService.markMessagesAsSeen(data.user2Id, data.user1Id); // Mark messages as seen
     return messages;
   }
+
+  //for typing
+ @SubscribeMessage('typing')
+handleTyping(
+  @MessageBody() data: { senderId: string; receiverId: string },
+  @ConnectedSocket() client: Socket,
+) {
+  this.server.to(data.receiverId).emit('userTyping', { senderId: data.senderId, typing: true });
+}
+
+@SubscribeMessage('stopTyping')
+handleStopTyping(
+  @MessageBody() data: { senderId: string; receiverId: string },
+  @ConnectedSocket() client: Socket,
+) {
+  this.server.to(data.receiverId).emit('userTyping', { senderId: data.senderId, typing: false });
+}
+
   
 
 
