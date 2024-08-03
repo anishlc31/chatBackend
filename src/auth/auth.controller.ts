20423a5f6f77ebbc513feb1148abf85c17268a63
Anish Lamichhane <codingonly31@gmail.com>
2024-08-03 21:24:46 +0545

    start of changing

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,42 +1,47 @@
 import { AuthService } from './auth.service';
-import { Body, Controller, HttpCode, HttpStatus, Post, Req, UseGuards,Request, Get, Query } from '@nestjs/common';
+import { Body, Controller, HttpCode, HttpStatus, Post, Req, UseGuards, Request, Get, Query, Res } from '@nestjs/common';
 import { loginDto, singupDto } from './dto';
-import { JwtAuthGuard } from './guards/jwt.guards';
-import { GetCurrentUserId  } from './decorator/user.decorator';
 import { Public } from './decorator/public.decorator';
+import { AuthGuard } from '@nestjs/passport';
 
 @Controller('auth')
 export class AuthController {
-
-
     constructor(private authService: AuthService) {}
 
 
+    @Get('google')
+    @UseGuards(AuthGuard('google'))
+    async googleAuth(@Req() req) {
+      // initiates the Google OAuth2 login flow
+    }
+  
+    @Get('google/callback')
+    @UseGuards(AuthGuard('google'))
+    async googleAuthRedirect(@Req() req, @Res() res) {
+      const tokens = await this.authService.googleLogin(req);
+      return res.redirect(`http://your-frontend-url?token=${tokens.access_token}`);
+    }
+
     @Public()
-    @Post ('/signup')
-    signup(@Body() dto  : singupDto){
+    @Post('/signup')
+    signup(@Body() dto: singupDto) {
         return this.authService.signup(dto);
     }
 
-    //login 
     @Public()
-    @Post ('/login')
-    login(@Body() dto  : loginDto){
+    @Post('/login')
+    login(@Body() dto: loginDto) {
         return this.authService.login(dto);
     }
 
-
-    @Get(':id')
-    @HttpCode(HttpStatus.OK)
-    @Post ('/logout')
-    logout(@GetCurrentUserId() userId: string){
-return this.authService.logout(userId)
-        
+    @Get('/find-by-username')
+    async findAllByUsername(@Query('username') username: string) {
+        return this.authService.findAllByUsername(username);
     }
 
-
-    @Get('/find-by-username')
-  async findAllByUsername(@Query('username') username: string) {
-    return this.authService.findAllByUsername(username);
-  }
+    // New endpoint to get all users
+    @Get('/users')
+    async getAllUsers() {
+        return this.authService.getAllUsers();
+    }
 }
