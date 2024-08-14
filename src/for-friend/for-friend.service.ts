f944d8ee678c4e7b4362bf54787b8e2945228a73
Anish Lamichhane <codingonly31@gmail.com>
2024-08-14 22:30:23 +0545

    shit

--- src/for-friend/for-friend.service.ts
+++ src/for-friend/for-friend.service.ts
@@ -2,11 +2,8 @@ import { Injectable } from '@nestjs/common';
 
 @Injectable()
 export class ForFriendService {
-    
-
-    addfriend(){
-        //let stop this drama and move on 
-    }
+  
 }
+//let's do
 
 
