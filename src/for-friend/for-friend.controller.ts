b4b4e5b91b11ef964d8d7f031c0b5dd76af49df3
Anish Lamichhane <codingonly31@gmail.com>
2024-08-26 23:06:04 +0545

    adding controller finally

--- src/for-friend/for-friend.controller.ts
+++ src/for-friend/for-friend.controller.ts
@@ -1,7 +1,26 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, Post, Req } from '@nestjs/common';
+import { ForFriendService } from './for-friend.service';
 
 @Controller('for-friend')
 export class ForFriendController {
 
-//comman anish 
+    constructor(private forFriend : ForFriendService) {}
+
+    @Post('/send-friend-request')
+    async sendFriendRequest(@Body('userId') userId: string, @Req() req: Request) {
+        const requesterId = req.user['id']; // Assuming you have JWT auth and user ID in token
+        return this.forFriend.sendFriendRequest(requesterId, userId);
+    }
+
+    @Post('/respond-friend-request')
+    async respondToFriendRequest(@Body() { friendshipId, status }: { friendshipId: string, status: string }) {
+        return this.forFriend.respondToFriendRequest(friendshipId, status);
+    }
+
+    @Get('/friends')
+    async getFriends(@Req() req: Request) {
+        const userId = req.user['id'];
+        return this.forFriend.getFriends(userId);
+    }
+
 }
