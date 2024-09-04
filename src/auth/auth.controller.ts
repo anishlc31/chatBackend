5f210c20cb5e61039c5c74d152d13b422afc1902
Anish Lamichhane <codingonly31@gmail.com>
2024-09-04 08:46:47 +0545

    start again

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,8 +1,9 @@
 import { AuthService } from './auth.service';
-import { Body, Controller, HttpCode, HttpStatus, Post, Req, UseGuards, Request, Get, Query, Res } from '@nestjs/common';
+import { Body, Controller, HttpCode, HttpStatus, Post, Req, UseGuards, Request, Get, Query, Res, Patch } from '@nestjs/common';
 import { loginDto, singupDto } from './dto';
 import { Public } from './decorator/public.decorator';
 import { AuthGuard } from '@nestjs/passport';
+import { FriendRequestDto, UpdateFriendshipStatusDto } from './dto/friend.dto';
 
 @Controller('auth')
 export class AuthController {
@@ -30,5 +31,18 @@ export class AuthController {
     async getAllUsers() {
         return this.authService.getAllUsers();
     }
+
+    @Public()
+    @Post('/add-friend')
+    async addFriend(@Body() friendRequestDto: FriendRequestDto) {
+      return this.authService.addFriend(friendRequestDto);
+    }
+
+
+    
+    @Public()
+    @Patch('/update-friendship-status')
+    async updateFriendshipStatus(@Body() updateFriendshipStatusDto: UpdateFriendshipStatusDto) {
+      return this.authService.updateFriendshipStatus(updateFriendshipStatusDto);
+    }
 }
-//fuck
