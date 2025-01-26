038b765eec1766914f75d085fff1c70e35a33ff7
Anish Lamichhane <codingonly31@gmail.com>
2025-01-27 02:03:43 +0545

    hello

---
+++ src/auth/strategy/jwt.strategy.ts
@@ -0,0 +1,19 @@
+import { Injectable } from '@nestjs/common';
+import { PassportStrategy } from '@nestjs/passport';
+import { ExtractJwt, Strategy } from 'passport-jwt';
+
+@Injectable()
+export class JwtStrategy extends PassportStrategy(Strategy, 'jwt') {
+    constructor() {
+        super({
+            jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(),
+            ignoreExpiration: false,
+            secretOrKey: process.env.JWT_SECRET,
+        });
+    }
+
+    async validate(payload: any) {
+        // Ensure 'sub' and other properties are returned
+        return { sub: payload.sub, email: payload.email };
+    }
+}
